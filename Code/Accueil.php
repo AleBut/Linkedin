145f2e59566c2912c2aacc573c81e995e0e14437
DESKTOP-RR5TSLB\Benjamin <benjamin.chardin@edu.ece.fr>
2018-05-01 11:16:39 +0200

    Amuse toi bien mon chou

---
+++ Code/Accueil.php
@@ -0,0 +1,5 @@
+<p>Bonjour !</p>
+
+<p>Votre email est <?php echo $_POST['email']; ?></p>
+<p>Votre mdp est <?php echo $_POST['MDP']; ?></p>
+<p>Si tu veux changer de prénom, <a href="Login.php">clique ici</a> pour revenir à la page formulaire.php.</p>
