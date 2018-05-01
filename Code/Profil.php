eea666580a917a7727bed65885c3ac14521067e7
AleBut <alexis/butin@edu.ece.fr>
2018-05-01 16:03:33 +0200

    Ajout profil et menu

--- Code/Profil.php
+++ Code/Profil.php
@@ -1,3 +1,12 @@
+<?php
+  // Démarre la session
+  session_start();
+  $ID_user = $_SESSION['ID_user'];
+
+  echo "Bienvenue dans votre accueil ! <br>";
+  echo "Votre ID est " . $ID_user;
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,47 +16,63 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
+    <!-- >WHAT MATE???? -->
     <title>Starter Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
+    <link href="Accueil.css" rel="stylesheet">
   </head>
 
   <body>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
+
+      <!-- Bouton à gauche -->
+      <a class="navbar-brand logo" href="#" >ECE'IN</a>
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+
+        <ul class="navbar-nav mr-auto -brand BarBoutons">
+          <!-- Bouton accueil -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="Accueil.php">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+            <!-- Bouton réseau -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Réseau</a>
+          </li>
+          <!-- Bouton emplois -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Emplois</a>
+          </li>
+          <!-- Bouton messagerie -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Messagerie</a>
+          </li>
+          <!-- Bouton notifications -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Notifications</a>
+          </li>
+          <!-- Bouton profil -->
+          <li class="nav-item active -brand bouton">
+            <a class="nav-link" href="Profil.php">Profil</a>
+          </li>
+          <!-- Bouton Deconnexion -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Deconnexion</a>
+          </li>
+        </ul>
       </div>
     </nav>
 
