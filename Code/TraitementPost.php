6fe81409f4f2bd74bb1d03513a74ccba61397bea
DESKTOP-RR5TSLB\Benjamin <benjamin.chardin@edu.ece.fr>
2018-05-01 14:56:33 +0200

    Créerlogin

---
+++ Code/TraitementPost.php
@@ -0,0 +1,8 @@
+<?php
+$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+//1. strrchr renvoie l'extension avec le point (« . »).
+//2. substr(chaine,1) ignore le premier caractère de chaine.
+//3. strtolower met l'extension en minuscules.
+$extension_upload = strtolower(  substr(  strrchr($_FILES['fichier']['name'], '.')  ,1)  );
+if ( in_array($extension_upload,$extensions_valides) ) echo "Extension correcte";
+?>
