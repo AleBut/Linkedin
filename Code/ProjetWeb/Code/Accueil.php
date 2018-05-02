1e0853f81468419bd4e2078709c3bd5d8df95319
DESKTOP-RR5TSLB\Benjamin <benjamin.chardin@edu.ece.fr>
2018-05-02 10:37:03 +0200

    Committraitementpost

---
+++ Code/ProjetWeb/Code/Accueil.php
@@ -0,0 +1,5 @@
+<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['email']; ?>  !</p>
+
+<p>Si tu veux changer de prénom, <a href="Login.php">clique ici</a> pour revenir à la page formulaire.php.</p>
--- Code/TraitementPost.php
+++ Code/TraitementPost.php
@@ -1,31 +1,20 @@
 <?php
+if($_FILES['fichier']['error'] == 0 && $_POST['ameliorer']=="Publiez ce que vous voulez" ){
+echo"qu'une photo";
 $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
 //1. strrchr renvoie l'extension avec le point (« . »).
 //2. substr(chaine,1) ignore le premier caractère de chaine.
 //3. strtolower met l'extension en minuscules.
 $extension_upload = strtolower(  substr(  strrchr($_FILES['fichier']['name'], '.')  ,1)  );
 if ( in_array($extension_upload,$extensions_valides) ) echo "Extension correcte";
-$nom = $_FILES['fichier']['name'];
+$nom = $_FILES['fichier']['name'];}
+if($_FILES['fichier']['error']>0 && $_POST['ameliorer']!="Publiez ce que vous voulez"  ){
+    echo "vous avec mis du texte mais pas de photo";
+}
+if($_FILES['fichier']['error']==0 && $_POST['ameliorer']!="Publiez ce que vous voulez"  ){
+    echo "texte et photo";
+}
+if($_FILES['fichier']['error']>0 && $_POST['ameliorer']=="Publiez ce que vous voulez"  ){
+    echo "t'as rien mis trouduc";
+}
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Starter Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-      <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
-  </head>
-
-  <body>
-      <img src=<?php echo $nom ?> width="310" height="155" />
-  </body>
-</html>
