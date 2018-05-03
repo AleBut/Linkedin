903b4fd734e7d34ee4f436d1d90376091a34f43a
Pierre <pierrot_du34@hotmail.fr>
2018-05-03 20:10:15 +0200

    modifier_formation_1.0
    Ajout de la modification/ajout/supression de formations
    Nav_bar & bouton retour sur tous les pages modif/ajout/supression

--- Code/ModifierExperiences.php
+++ Code/ModifierExperiences.php
@@ -63,8 +63,57 @@
   </head>
 
   <body class="text-center">
+      
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+
+      <!-- Bouton à gauche -->
+      <a class="navbar-brand logo" href="#" >ECE'IN</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <ul class="navbar-nav mr-auto -brand BarBoutons">
+          <!-- Bouton accueil -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="Accueil.php">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+            <!-- Bouton réseau -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="AfficherAmis.php">Réseau</a>
+          </li>
+          <!-- Bouton emplois -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Emplois</a>
+          </li>
+          <!-- Bouton messagerie -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="Messagerie.php">Messagerie</a>
+          </li>
+          <!-- Bouton notifications -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="#">Notifications</a>
+          </li>
+          <!-- Bouton profil ACTIVE -->
+          <li class="nav-item active -brand bouton">
+            <a class="nav-link" href="Profil.php">Profil</a>
+          </li>
+          <!-- Bouton Deconnexion -->
+          <li class="nav-item -brand bouton">
+            <a class="nav-link" href="Deconnexion.php">Deconnexion</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      
     <form class="form-signin" action="ModifierExperiences.php" method="post">
-      <h1 class="h3 mb-3 font-weight-normal">Modifier vos Experiences</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Vos Experiences</h1>
          
         <?php for($i=0;$i<$cpt;$i++){ ?>
         <div class="card text-center text-white bg-secondary mb-3" style="width: 500px; margin-left: -100px">
@@ -78,7 +127,7 @@
         </div>
         <?php } ?>
     <a href="AjouterExperience.html" class="btn btn-primary">Ajouter une Experience</a><br><br>
-    <a class="btn btn-lg btn-block btngr egn " href="AfficherModifierProfil.php">Retour</a>
+    <a class="btn btn-info btn-lg btn-block egn " href="AfficherModifierProfil.php">Retour</a>
     <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
       
--- Code/ModifierUneFormation.php
+++ Code/ModifierUneFormation.php
@@ -3,15 +3,6 @@
 	session_start();
 	$ID_user    = $_SESSION['ID_user'];
 
-    //Arrays internes
-    $arrayID_user = array();
-    $arrayID_formation = array();
-    $arrayTypeFormation = array();
-    $arrayNomEcole = array();
-    $arrayDateArrive = array();
-    $arrayDateFin = array();
-    $arrayCommentaires = array();
-    $cpt = 0;
 	// Identifier BDD
 	$database = "linkedin";
 
@@ -24,29 +15,28 @@
 	if($db_found)
 	{ 
         // On écrit la requête SQL
-		$sql2 = "SELECT * FROM formation WHERE ID_user = " .  $ID_user . "";
-
+		$sql = "SELECT * FROM formation WHERE ID_formation = " .$_GET['ID'];
 		// On envoit la requête
-		$result2 = mysqli_query($db_handle, $sql2);
-
-		// Si le tableau de resultat est vide
-        while($data2 = mysqli_fetch_assoc($result2))    
-	    {	          
-            array_push($arrayID_user,$data2['ID_user']);
-            array_push($arrayID_formation,$data2['ID_formation']);
-            array_push($arrayTypeFormation,$data2['TypeFormation']);
-            array_push($arrayNomEcole,$data2['NomEcole']);
-            array_push($arrayDateArrive,$data2['DateArrive']);
-            array_push($arrayDateFin,$data2['DateFin']);
-            array_push($arrayCommentaires,$data2['Commentaire']);
-            $cpt++;
-	    }
-
+        $result = mysqli_query($db_handle, $sql);
+        $data=mysqli_fetch_assoc($result);
+        if(empty($data)){
+           echo"error";
+            }
+        
+        else{
+            $TypeFormation = $data['TypeFormation'];
+            $NomEcole = $data['NomEcole'];
+            $DateArrive = $data['DateArrive'];
+            $DateFin = $data['DateFin'];
+            $Commentaires = $data['Commentaire'];
+            }
 	}
-?> 
+?>
+           
+<!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">  
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -60,7 +50,7 @@
     <link href="signin.css" rel="stylesheet">
   </head>
 
-  <body class="text-center">
+  <body class="text-center" >
       
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 
@@ -110,25 +100,28 @@
       </div>
     </nav>
       
-    <form class="form-signin" action="ModifierFormations.php" method="post">
-      <h1 class="h3 mb-3 font-weight-normal">Vos Formations</h1>
-         
-        <?php for($i=0;$i<$cpt;$i++){ ?>
-        <div class="card text-center text-white bg-secondary mb-3" style="width: 500px; margin-left: -100px">
-          <div class="card-header"><?php echo $arrayTypeFormation[$i]?> à <?php echo $arrayNomEcole[$i]?> </div>
-          <div class="card-body">
-            <p class="card-text">Du : <?php echo $arrayDateArrive[$i]?> au <?php echo $arrayDateFin[$i]?></p>
-            <p class="card-text">Commentaires : <?php echo $arrayCommentaires[$i]?></p>
-            <a href="SupprimerFormation.php?ID=<?php echo $arrayID_formation[$i]?>" class="btn btn-primary">Supprimer</a>
-            <a href="ModifierUneFormation.php?ID=<?php echo $arrayID_formation[$i]?>" class="btn btn-primary">Modifier</a>
-          </div>
-        </div>
-        <?php } ?>
-    <a href="AjouterFormation.html" class="btn btn-primary">Ajouter une Formation</a><br><br>
-    <a class="btn btn-info btn-lg btn-block egn " href="AfficherModifierProfil.php">Retour</a>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    <form class="form-signin" action="AppliquerModifFormation.php?ID=<?php echo $_GET['ID'] ?>" method="post">
+      <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Modifier votre Formation</h1>
+      <label for="inputText" class="sr-only">Type d'Formation</label>
+      <input type="text" class="form-control" placeholder="Type de Formation" value="<?php echo $TypeFormation?>" required autofocus name="TypeFormation">
+        <br>
+      <label for="inputText" class="sr-only">NomEcole</label>
+      <input type="text" class="form-control" placeholder="Nom de l'Ecole" value="<?php echo $NomEcole?>" required autofocus name="NomEcole">
+        <br>
+      <input type="date" name="DateArrive" placeholder="Date d'arrivee" value="<?php echo $DateArrive?>" style="text-align : right;">
+        <br><br>
+      <input type="date" name= "DateFin" placeholder="Date de fin" value="<?php echo $DateFin?>" style="text-align : right;">
+        <br><br>
+      <label for="inputText" class="sr-only">Commentaires</label>
+      <input type="text" class="form-control" placeholder="Commentaires" value="<?php echo $Commentaires?>" required autofocus name="Commentaires">
+        <br>
+        
+      <button class="btn btn-success btn-lg btn-block btngr egn " type="submit">Valider</button>
+      <a class="btn btn-info btn-lg btn-block egn " href="ModifierFormations.php">Retour</a>
+
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
       
   </body>
-
 </html>
