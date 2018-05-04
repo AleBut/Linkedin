2b4a89cef7a81fff6c9d465a1cff423511aa6e69
Alexis <alexis.butin@edu.ece.fr>
2018-05-04 12:42:18 +0200

    Ajout post navigation

--- Code/TraitementPost.php
+++ Code/TraitementPost.php
@@ -1,21 +1,42 @@
 <?php
-if($_FILES['fichier']['error'] == 0 && $_POST['ameliorer']=="Publiez ce que vous voulez" ){
-    $img=$_FILES['fichier']['name'];
-    echo '<div><img src='.$img.' alt="" height="200" width"220"/></div>';
-$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
-//1. strrchr renvoie l'extension avec le point (« . »).
-//2. substr(chaine,1) ignore le premier caractère de chaine.
-//3. strtolower met l'extension en minuscules.
-$extension_upload = strtolower(  substr(  strrchr($_FILES['fichier']['name'], '.')  ,1)  );
-if ( in_array($extension_upload,$extensions_valides) ) echo "Extension correcte";
-$nom = $_FILES['fichier']['name'];}
-if($_FILES['fichier']['error']>0 && $_POST['ameliorer']!="Publiez ce que vous voulez"  ){
-    echo '<div style="width : 400px;"><p style="text-align : center;">'.$_POST['ameliorer'].'</p></div>';
-}
-if($_FILES['fichier']['error']==0 && $_POST['ameliorer']!="Publiez ce que vous voulez"  ){
-    echo '<div style="width : 400px;"><p style="text-align : center;">'.$_POST['ameliorer'].'</p><img src='.$_FILES['fichier']['name'].' alt="" height="200" width"220"/></div>';
-}
-if($_FILES['fichier']['error']>0 && $_POST['ameliorer']=="Publiez ce que vous voulez"  ){
-    echo "t'as rien mis trouduc";
-}
+
+	session_start();
+	$ID_user    = $_SESSION['ID_user'];
+
+	// Identifier BDD
+	$database = "linkedin";
+	// Connecter utilisateur à MYSQL
+	$db_handle = mysqli_connect('localhost', 'root', '');
+	// Connecter l'utilisateur à la BDD
+	$db_found = mysqli_select_db($db_handle, $database);
+
+	$message = isset($_POST['ameliorer'])? $_POST['ameliorer']: "";
+
+	if($_FILES['fichier']['error'] == 0 && $message =="Publiez ce que vous voulez" ) // Photo
+	{
+	    $contenu = '<div><img src=' . $_FILES['fichier']['name'] . ' alt="" height="200" width"220" style="margin-left : 50px;"/></div>';
+	}
+	if($_FILES['fichier']['error']>0 && $message !="Publiez ce que vous voulez"  ) // texte
+	{
+	    $contenu = '<div style="width : 400px;"><p style="text-align : center;">'. $message .'</p></div>';
+	}
+	if($_FILES['fichier']['error']==0 && $message !="Publiez ce que vous voulez"  ) // texte et photo
+	{
+	    $contenu = '<div style="width : 400px;"><p style="text-align : center;">'. $message .'</p><img src='.$_FILES['fichier']['name'].' alt="" height="200" width"220" style="margin-left : 50px;"/></div>';
+	}
+	if($_FILES['fichier']['error']>0 && $message =="Publiez ce que vous voulez"  )
+	{
+	    $contenu = "";
+	}
+
+	// Si BDD existe
+	  if( $db_found && (strlen($contenu) > 0) )
+	  {
+	    // Requete table post
+	    $sql = "INSERT INTO post (ID_user, Contenu, DatePublication, Lieu, ModeVisibilite) VALUES ('" . $ID_user ."', '". $contenu ."', CURRENT_TIMESTAMP, 'Paris', '0')";
+	    mysqli_query($db_handle, $sql);
+	}
+	mysqli_close($db_handle);
+
+  	header("Refresh: 0; url=Accueil.php");
 ?>
